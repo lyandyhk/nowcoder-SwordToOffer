f435c32f32b14432a0fdb7328950a5be72dd3b41
Febiven <Febiven@100.66.3.246>
2017-09-19 10:12:45 +0800

    Mirror of binary tree

---
+++ src/problemList/Q18.java
@@ -0,0 +1,45 @@
+package problemList;
+/**
+ * 
+ * @author Febiven
+ *题目：操作给定的二叉树，将其变换为源二叉树的镜像。
+	输入描述:
+	二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+ */
+public class Q18 {
+	public void Mirror(TreeNode root) {
+        if(root == null)
+            return;
+        if(root.left != null || root.right != null){
+        	TreeNode temp = root.left;
+        	root.left = root.right;
+        	root.right = temp;
+        	Mirror(root.left);
+        	Mirror(root.right);
+        }
+    }
+}
+/**
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
